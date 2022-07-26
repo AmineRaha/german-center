25b97adf3d8660d2a21767279a6e7db1ec16f892
AmineRaha <amineraha2@gmail.com>
2022-07-26 22:11:29 +0100

    home anim

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,17 @@
 import { Box, Stack, Typography } from '@mui/material'
 import React from 'react'
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
     <Stack alignItems="center" height={"89vh"} justifyContent={"center"} sx={{backgroundImage: `url(${"https://picsum.photos/1920/1080"})`}}  >
-      <Typography variant='h1'>German Center</Typography>
+      <motion.div
+        initial={{ y:'20', opacity: 0}}
+        animate={{y:0, opacity: 1}}
+        transition={{ duration: 1.2 }}
+      >
+        <Typography variant='h1' sx={{ fontWeight: "bold"}}>German Center</Typography>
+      </motion.div>
     </Stack>
   )
 }
