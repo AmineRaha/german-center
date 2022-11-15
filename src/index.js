a6c629f46028417ee21f0584923fb6b3b57775df
AmineRaha <amineraha2@gmail.com>
2022-11-15 14:43:21 +0100

    fix?

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
+<<<<<<< Updated upstream
+=======
+import bg from "./assets/images/pictures/_ABP3387.jpg";
+
+>>>>>>> Stashed changes
 import App from './App';
 import "./index.css";
 import "./i18n";
